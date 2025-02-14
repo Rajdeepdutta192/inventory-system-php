228f9f2a95345634b408508d4ac459d70d9b3f64
Rajdeep Dutta <34247617+Rajdeepdutta192@users.noreply.github.com>
2025-02-15 00:29:27 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,101 @@
+<?php
+session_start();
+include 'inc/dbcon.php';
+if (isset($_SESSION['IS_LOGIN']) && $_SESSION['IS_LOGIN'] === 'yes') {
+    redirect('index.php');
+}
+$msg = '';
+if (isset($_POST['submit'])) {
+    $username = mres($con, $_POST['username']);
+    $password = mres($con, $_POST['password']);
+
+    $sql = "select * from admin where username = '{$username}' and password = '{$password}'";
+    $res = mysqli_query($con, $sql);
+    if (mysqli_num_rows($res) > 0) {
+        $row = mysqli_fetch_assoc($res);
+        $_SESSION['NAME'] = $row['username'];
+        $_SESSION['GODOWN1'] = 'godown1';
+        // $msg = 'yes';
+        redirect('index.php');
+    } else {
+        $msg = "Invalid user";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Kritidhara | Log in</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="dist/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <div class="login-logo">
+            <a href="index2.html"><b>Admin</b></a>
+        </div>
+        <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="login-box-msg"><a href="../">Go to main menu</a></p>
+
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" name="username" placeholder="Username">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" name="password" placeholder="Password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <!-- /.col -->
+                        <div class="col-4">
+                            <p style="background-color: red; color:white">
+                                <?php
+                                if ($msg != '') {
+                                    echo $msg;
+                                }
+                                ?>
+                            </p>
+                        </div>
+                        <div class="col-4">
+                            <button type="submit" name="submit" class="btn btn-primary btn-block">Sign In</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+
+            </div>
+        </div>
+        <!-- /.login-box -->
+
+        <!-- jQuery -->
+        <script src="plugins/jquery/jquery.min.js"></script>
+        <!-- Bootstrap 4 -->
+        <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- AdminLTE App -->
+        <script src="dist/js/adminlte.min.js"></script>
+</body>
+
+</html>
